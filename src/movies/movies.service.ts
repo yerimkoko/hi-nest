df92fea0ceea6ae0bf47f967966bd36c6e134cb6
yerimkoko <gochi97@naver.com>
2021-01-13 11:46:05 +0900

    Movies Service Part.II

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,12 +10,16 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === parseInt(id)); //+id와 같은 뜻.
+    const movie = this.movies.find((movie) => movie.id === parseInt(id)); //+id와 같은 뜻.
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID: ID ${id} not found.`);
+    }
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
+  deleteOne(id: string) {
+    this.getOne(id);
     this.movies.filter((movie) => movie.id !== +id);
-    return true;
   }
 
   create(movieData) {
